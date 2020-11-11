c0ece8ae03c0377ce4992d1bc108251dfc37231e
clare0901 <clarerebello0901@gmail.com>
2020-11-11 19:03:03 +0530

    add

--- instagram_clone/resources/views/posts/allposts.blade.php
+++ instagram_clone/resources/views/posts/allposts.blade.php
@@ -5,7 +5,9 @@
 <div class="text-center">
 @foreach($posts as $post)
     <div class="" style="display:inline">
-        <a href="/profile/{{$post->user->id}}"><img src="storage/{{$post->image}}" class="w-25" alt="pic"></a>
+        <!-- <a href="/profile/{{$post->user->id}}"> -->
+            <img src="storage/{{$post->image}}" class="w-25" alt="pic">
+        <!-- </a> -->
     </div>
 @endforeach
 </div>
