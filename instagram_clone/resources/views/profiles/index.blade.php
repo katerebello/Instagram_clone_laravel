f614510553e211de5f2abd8b24d05976ceb9ce0f
clare0901 <clarerebello0901@gmail.com>
2020-11-16 17:19:21 +0530

    update

--- instagram_clone/resources/views/profiles/index.blade.php
+++ instagram_clone/resources/views/profiles/index.blade.php
@@ -3,39 +3,71 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3">
-            <img src="{{ $user->profile->profileImage() }}" class="rounded-circle w-100" alt=""><!--here profileImage is a method we wrote in profiles model to get default image if not provided oresle the one provided-->
+        <div class="col-4 pt-4 pr-5 pl-5 pb-5">
+            <img src="{{ $user->profile->profileImage() }}" style="border-radius:50%;" class="w-100">
+            <!--here profileImage is a method we wrote in profiles model to get default image if not provided oresle the one provided-->
         </div>
-        <div class="col-9">
-            <div class="d-flex justify-content-between align-items-baseline">
-                <div class="d-flex align-items-center pb-3">
-                    <div class="h4">{{ $user->username }}</div>
+        <div class="col-8 pt-5">
+            <div class="d-flex justify-content-between align-items-baseline pt-4">
+                <div class="d-flex pb-3">
+                    <h1>{{ $user->username }}</h1>
                     
-                    <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button><!--will refer to the FollowButton.vue--><!--the user-id here is passed to the .vue file-->
+                    <!-- follow button -->
+                    <!-- this will create a view for us in FollowButton.vue -->
+                    <!-- give the user id through property -->
+                    @can('view', $user->profile)
+                    <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>
+                    @endcan
                 </div>
 
-                @can ('update', $user->profile)
-                    <a href="/p/create">Add New Post</a>
+                <!-- if the user has the authorization to update the only the user will be allowed to add posts -->
+                @can('update', $user->profile)
+                <a href="/p/create" style="color:black; text-decoration:none;">
+                    <i class="fas fa-plus" style="font-size: 15px;"></i>
+                    Add New Post
+                </a>
                 @endcan
-               
+                
             </div>
           
+            <div class="d-flex">
+                <div class="pr-4"> 
+                    <a href="#posts" style="text-decoration: none; color:black;"><strong class="pr-2">{{$user->posts->count()}}</strong>Posts</a> 
+                </div>
+                <div class="pr-4"> 
+                    <a href="/profile/{{ $user->id }}/followers" style="text-decoration: none; color:black;"><strong class="pr-2">
+                        {{ $user->profile->followers->count()}}</strong>followers
+                    </a> 
+                </div>
+                <div class="pr-4">
+                    <a href="/profile/{{ $user->id }}/following" style="text-decoration: none; color:black;"><strong class="pr-2">  
+                        {{ $user->following->count()}}</strong>following
+                    </a> 
+                </div>
+            </div>
 
-            @can ('update', $user->profile)
-                <a href="/profile/{{ $user->id }}/edit">edit profile</a>
+            <!-- edit profile -->
+            @can('update', $user->profile)
+            <div class="pt-3 d-flex justify-content-end">
+                <i class="fas fa-user-edit pt-1 pr-1"  style="font-size: 15px;"></i>
+                <a href="/profile/{{ $user->id }}/edit" style="color:black; text-decoration:none;">Edit Profile</a>
+            </div>
             @endcan
 
-            <div class="d-flex">
-            <div class="pr-5"> <a href="#posts"><strong>{{$user->posts->count()}}</strong>Posts</a> </div>
-            <div class="pr-5"> <a href="/profile/{{ $user->id }}/followers"><strong>{{ $user->profile->followers->count()}}</strong>followers</a> </div>
-            <div> <a href="/profile/{{ $user->id }}/following"><strong>{{ $user->following->count()}}</strong>following</a> </div>
-            </div>
+            <!-- title -->
             <div class="pt-3 font-weight-bold">{{ $user->profile->title }}</div>
+
+            <!-- description -->
             <div>{{ $user->profile->description }}</div>
-            <a href="{{ $user->profile->url }}">{{ $user->profile->url ?? 'N/A' }}
-            </a>
+            
+            <!-- url -->
+            <div>
+                <a href="{{ $user->profile->url }}">{{ $user->profile->url ?? 'N/A' }}</a>
+            </div>
         </div>
     </div>
+
+
     <!--user posts-->
     <div class="row pt-5" id="posts">
         @foreach($user->posts as $post)
