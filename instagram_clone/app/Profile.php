6b0bf5ccc2b4d0560985d2a7a772f9449e6d5911
clare0901 <clarerebello0901@gmail.com>
2021-02-09 19:26:17 +0530

    Update Profile.php

--- instagram_clone/app/Profile.php
+++ instagram_clone/app/Profile.php
@@ -10,7 +10,9 @@ class Profile extends Model
 
     public function profileImage()
     {
+        // $imagePath = ($this->image) ?  $this->image : 'profile/7wbefrd2tRPfctIqQ8LE8GHJoAbtU4LjA0z0Yvjq.jpeg' ; 
         $imagePath = ($this->image) ?  $this->image : 'profile/7wbefrd2tRPfctIqQ8LE8GHJoAbtU4LjA0z0Yvjq.png' ; 
+
         return '/storage/'. $imagePath;//in model $this implies the model u are in ,in this case profile
        //else can also take storage outside like follows:
        //'/storage/' return ($this->image) ?   .$this->image : '/storage/profile/7wbefrd2tRPfctIqQ8LE8GHJoAbtU4LjA0z0Yvjq.png'  //means if image provided take the option before : ,else if not take after : one
